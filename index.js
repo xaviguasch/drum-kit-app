d4c89274ce29878e888b50b1a58ccd21dc68b849
xaviguasch <xaviguasch@gmail.com>
2019-02-02 16:02:22 +0100

    finishing the sound implementation via switch statements

--- index.js
+++ index.js
@@ -4,12 +4,51 @@ const drumButtons = document.querySelectorAll('.drum')
 //     button.addEventListener('click', handleClick)
 // })
 
-const audio = new Audio('./sounds/tom-1.mp3')
 
 for (let i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener('click', function () {
-        audio.play()
-        console.log('I got clicked!')
+        const buttonInnerHTML = this.innerHTML
 
+        switch (buttonInnerHTML) {
+            case "w":
+                const tom1 = new Audio('./sounds/tom-1.mp3')
+                tom1.play()
+                break;
+
+            case "a":
+                const tom2 = new Audio('./sounds/tom-2.mp3')
+                tom2.play()
+                break;
+            case "s":
+                const tom3 = new Audio('./sounds/tom-3.mp3')
+                tom3.play()
+                break;
+            case "d":
+                const tom4 = new Audio('./sounds/tom-4.mp3')
+                tom4.play()
+                break;
+            case "j":
+                const snare = new Audio('./sounds/snare.mp3')
+                snare.play()
+                break;
+            case "k":
+                const crash = new Audio('./sounds/crash.mp3')
+                crash.play()
+                break;
+            case "l":
+                const kick = new Audio('./sounds/kick-bass.mp3')
+                kick.play()
+                break;
+
+            default:
+        }
     })
 }
+
+
+
+
+
+
+// audio.play()
+// console.log('I got clicked!')
