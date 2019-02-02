d54a4ef047428444bac276ef56b94b1aea0ac2e3
xaviguasch <xaviguasch@gmail.com>
2019-02-02 16:46:51 +0100

    adding keyboard event listeners and refactoring makeSound()

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// DETECTING BUTTON PRESSED
+
 const drumButtons = document.querySelectorAll('.drum')
 
 // drumButtons.forEach((button) => {
@@ -8,47 +10,53 @@ const drumButtons = document.querySelectorAll('.drum')
 for (let i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener('click', function () {
         const buttonInnerHTML = this.innerHTML
-
-        switch (buttonInnerHTML) {
-            case "w":
-                const tom1 = new Audio('./sounds/tom-1.mp3')
-                tom1.play()
-                break;
-
-            case "a":
-                const tom2 = new Audio('./sounds/tom-2.mp3')
-                tom2.play()
-                break;
-            case "s":
-                const tom3 = new Audio('./sounds/tom-3.mp3')
-                tom3.play()
-                break;
-            case "d":
-                const tom4 = new Audio('./sounds/tom-4.mp3')
-                tom4.play()
-                break;
-            case "j":
-                const snare = new Audio('./sounds/snare.mp3')
-                snare.play()
-                break;
-            case "k":
-                const crash = new Audio('./sounds/crash.mp3')
-                crash.play()
-                break;
-            case "l":
-                const kick = new Audio('./sounds/kick-bass.mp3')
-                kick.play()
-                break;
-
-            default:
-        }
+        makeSound(buttonInnerHTML)
     })
 }
 
 
+// DETECTING KEYBOARD PRESSED
+
+document.addEventListener('keydown', function (event) {
+    makeSound(event.key)
+})
 
 
 
 
-// audio.play()
-// console.log('I got clicked!')
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            const tom1 = new Audio('./sounds/tom-1.mp3')
+            tom1.play()
+            break;
+        case "a":
+            const tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()
+            break;
+        case "s":
+            const tom3 = new Audio('./sounds/tom-3.mp3')
+            tom3.play()
+            break;
+        case "d":
+            const tom4 = new Audio('./sounds/tom-4.mp3')
+            tom4.play()
+            break;
+        case "j":
+            const snare = new Audio('./sounds/snare.mp3')
+            snare.play()
+            break;
+        case "k":
+            const crash = new Audio('./sounds/crash.mp3')
+            crash.play()
+            break;
+        case "l":
+            const kick = new Audio('./sounds/kick-bass.mp3')
+            kick.play()
+            break;
+
+        default:
+            console.log(key)
+    }
+
+}
